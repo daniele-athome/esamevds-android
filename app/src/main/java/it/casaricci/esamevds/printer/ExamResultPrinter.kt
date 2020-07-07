0b8108e994f9fac7401e19d5c9956d8ee1917619
Daniele Ricci <daniele@casaricci.it>
2020-07-07 14:49:59 +0200

    Update copyright notice
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/src/main/java/it/casaricci/esamevds/printer/ExamResultPrinter.kt
+++ app/src/main/java/it/casaricci/esamevds/printer/ExamResultPrinter.kt
@@ -1,6 +1,6 @@
 /*
  * EsameVDS Android app
- * Copyright (c) 2019 Daniele Ricci
+ * Copyright (c) 2020 Daniele Ricci
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
