b384891a27e1dc7d0b7a013ea7916b3ad50cac90
Daniele Ricci <daniele@casaricci.it>
2020-08-14 23:55:22 +0200

    Preparing a new game
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/src/main/java/it/casaricci/esamevds/ui/CompassTrainingActivity.kt
+++ app/src/main/java/it/casaricci/esamevds/ui/CompassTrainingActivity.kt
@@ -18,35 +18,25 @@
 
 package it.casaricci.esamevds.ui
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import it.casaricci.esamevds.BuildConfig
 import it.casaricci.esamevds.R
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class CompassTrainingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        button_compass_training.setOnClickListener {
-            CompassTrainingActivity.start(this)
-        }
-        button_icao_alphabet.setOnClickListener {
-            IcaoAlphabetActivity.start(this)
-        }
-
-        text_version.text = getString(
-            R.string.text_version,
-            BuildConfig.VERSION_NAME,
-            BuildConfig.VERSION_CODE
-        )
+        setContentView(R.layout.activity_compass_training)
     }
 
-    fun select(view: View) {
-        val specialty = (view.tag as String).toInt()
-        GameChoiceActivity.start(this, specialty)
+    companion object {
+
+        fun start(context: Context) {
+            val i = Intent(context, CompassTrainingActivity::class.java)
+            context.startActivity(i)
+        }
     }
+
 }
