6b1b974a49e3cd28775d3ce8d3d861c35ec2baee
Daniele Ricci <daniele@casaricci.it>
2020-08-29 10:21:36 +0200

    Load random game
    Signed-off-by: Daniele Ricci <daniele@casaricci.it>

--- app/src/main/java/it/casaricci/esamevds/ui/CompassTrainingActivity.kt
+++ app/src/main/java/it/casaricci/esamevds/ui/CompassTrainingActivity.kt
@@ -22,29 +22,46 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import it.casaricci.esamevds.R
+import java.util.*
 
 class CompassTrainingActivity : AppCompatActivity(), CompassTrainingContainer {
+
+    private val factory: GameFactory = GameFactory()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compass_training)
 
-        // TEST
-        val f = CTGuessDegreesFragment.newInstance()
-        f.container = this
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.train_fragment, f)
-            .commit()
+        newGame()
     }
 
     override fun onGameCompleted() {
-        // TEST
-        val f = CTDoaFragment.newInstance()
+        newGame()
+    }
+
+    private fun newGame() {
+        val f = factory.getInstance()
         f.container = this
         supportFragmentManager.beginTransaction()
-            .replace(R.id.train_fragment, f)
+            .replace(R.id.train_fragment, f as Fragment)
             .commit()
+    }
+
+    class GameFactory {
+
+        fun getInstance() : CompassTrainingFragment {
+            return when(Random().nextInt(NUM_GAMES)) {
+                0 -> CTGuessDegreesFragment.newInstance()
+                1 -> CTDoaFragment.newInstance()
+                else -> throw RuntimeException("Impossible")
+            }
+        }
+
+        companion object {
+            private const val NUM_GAMES = 2
+        }
     }
 
     companion object {
